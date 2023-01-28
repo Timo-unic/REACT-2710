b3ec5590d257ed7d3b9b2f348827d2e07d75f776
Timofii <unicondor.tv@gmail.com>
2023-01-28 02:20:19 +0200

    create Component and Export default

---
+++ src/Content.tsx
@@ -0,0 +1,13 @@
+type Props = {
+    text1: string
+    text2: string
+    year: number
+}
+const Content = (props: Props) => {
+  return  <React.Fragment>
+  <p>{props.text1}</p>
+  <p>{props.text2}</p>
+  <div>Year: {props.year}</div>
+</React.Fragment>
+}
+export default Content
--- src/index.tsx
+++ src/index.tsx
@@ -1,36 +1,18 @@
 import { type } from 'os'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Content from './Content'
 
 interface ITitleProps {
     text: string
 }
-type ContentProps = {
-    text1: string
-    text2: string
-    year: number
 
-}
 
 const Title = (props: ITitleProps) => {
     console.log(props)
     return <h1>Hello {props.text}</h1>
 }
 
-const Content = (props: ContentProps) => {
-    console.log(props)
-    return (
-        <React.Fragment>
-            <p>
-                {props.text1}
-            </p>
-            <p>
-                {props.text2}
-            </p>
-            <div>Year: {props.year}</div>
-        </React.Fragment>
-    )
-}
 
 function App() {
     return (
