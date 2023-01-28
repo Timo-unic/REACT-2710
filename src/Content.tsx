928efb5121eae907095ca692bb235807c71dba7b
Timofii <unicondor.tv@gmail.com>
2023-01-29 01:38:07 +0200

    another case for connecting style.css

--- src/Content.tsx
+++ src/Content.tsx
@@ -1,17 +1,13 @@
+import './Content.css'
+
 type Props = {
     text1: string
     text2: string
     year: number
 }
 export const Content = (props: Props) => {
-  let data = true
   return  <>
-  <p style={{
-    color: data ? 'yellow' : 'white',
-    background: 'green',
-    padding: '15px',
-
-  }}>{props.text1}</p>
+  <p className="content">{props.text1}</p>
   <p>{props.text2}</p>
   <div>Year: {props.year}</div>
 </>
