cf2481c62ac37953afaac04b8e02f935ca484b25
Timofii <unicondor.tv@gmail.com>
2023-01-29 03:59:41 +0200

    add files App, Header, Main, Footer

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
-import { type } from 'os'
+import App from 'container/App/App'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 
-
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode></React.StrictMode>)
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
