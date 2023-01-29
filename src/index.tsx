51cb3ff16c2bccea35a1ac37e26075c5e44d0c5c
Timofii <unicondor.tv@gmail.com>
2023-01-29 02:35:18 +0200

    remove files and add absolute import

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,8 @@
 import { type } from 'os'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {Content} from './Content'
-import Title from './Title'
 
 
-function App() {
-    return (
-        <>
-            <Title text='React' />
-            <Title text='TS' />
-            <Content 
-            text1='saepe sit voluptates minima voluptate illo sed, necessitatibus earum animi totam error eveniet.' 
-            text2='saepe sit voluptates minima voluptate illo sed, necessitatibus earum animi totam error eveniet.' 
-            year={2023}/>
-        </>
-    )
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-)
+root.render(<React.StrictMode></React.StrictMode>)
