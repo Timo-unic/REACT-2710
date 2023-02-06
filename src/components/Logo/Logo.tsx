84c664175fb93945270e176a7212cf08de08e7d6
Timofii <unicondor.tv@gmail.com>
2023-02-06 21:33:15 +0200

    add CartHeader

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -3,10 +3,10 @@ import Typography from '@mui/material/Typography'
 
 type Props = {}
 const Logo = (props: Props) => {
-  return (
-    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-    <img src={logo} alt="fake shop" />
-    </Typography>
-  )
+    return (
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <img src={logo} alt="fake shop" />
+        </Typography>
+    )
 }
 export default Logo
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import App from 'container/App/App'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
     <React.StrictMode>
