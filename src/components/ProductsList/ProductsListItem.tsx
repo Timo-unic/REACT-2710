3d2683b945c6de7428234af71de31f97c4a7ae72
Timofii <unicondor.tv@gmail.com>
2023-02-11 01:57:54 +0200

    methods increment and decremend are implemented

--- src/components/ProductsList/ProductsListItem.tsx
+++ src/components/ProductsList/ProductsListItem.tsx
@@ -23,13 +23,19 @@ class ProductsListItem extends Component<Props, State> {
             count: 1,
         }
         this.onIncrementClick = this.onIncrementClick.bind(this)
+        this.onDecrementClick = this.onDecrementClick.bind(this)
     }
 
     onIncrementClick() {
-        console.log(this)
-        this.setState({
-            count: 2,
-        })
+        this.setState((prevState: State) => ({
+            count: prevState.count + 1,
+        }))
+    }
+
+    onDecrementClick() {
+        this.setState((prevState: State) => ({
+            count: prevState.count - 1,
+        }))
     }
 
     render() {
@@ -46,7 +52,7 @@ class ProductsListItem extends Component<Props, State> {
                     <div className="product-features">Capacity: {capacity} Gb</div>
                     <div className="product-price">{price}$</div>
                     <div className="product-quantity">
-                        <Button variant='outlined'>-</Button>
+                        <Button variant='outlined' onClick={this.onDecrementClick}>-</Button>
                         <TextField size='small' value={this.state.count} />
                         <Button variant='outlined' onClick={this.onIncrementClick}>+</Button>
                     </div>
