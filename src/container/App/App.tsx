f632773cf070464f747729c89aa3496407be3247
Timofii <unicondor.tv@gmail.com>
2023-02-21 23:53:07 +0200

    add CartProductList, CartProductListItem, CartProductListExtended and CartTotal

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -38,7 +38,7 @@ const App = (props: Props) => {
                         path="/"
                         element={<Home addProductToCart={addProductToCart} />}
                     />
-                    <Route path="cart" element={<CartPage />} />
+                    <Route path="cart" element={<CartPage productsInCart={ProductsInCart} />} />
                 </Routes>
             </Container>
             <Footer />
