2ec8a9ed1d5f5cac02243b8f800d5490ad57efe6
Timofii <unicondor.tv@gmail.com>
2023-03-05 04:03:06 +0200

    add CheckoutPage

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -6,6 +6,7 @@ import { Container } from '@mui/material'
 import { Route, Routes } from 'react-router-dom'
 import Home from 'pages/Home/Home'
 import CartPage from 'pages/Cart/CartPage'
+import CheckoutPage from 'pages/Checkout/CheckoutPage'
 
 type Props = {}
 
@@ -18,6 +19,7 @@ const App = (props: Props) => {
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="cart" element={<CartPage />} />
+                    <Route path="checkout" element={<CheckoutPage />} />
                 </Routes>
             </Container>
             <Footer />
--- src/pages/Cart/CartPage.tsx
+++ src/pages/Cart/CartPage.tsx
@@ -2,6 +2,7 @@ import { Grid, Typography } from '@mui/material'
 import CartProductList from 'components/CartProductList/CartProductList'
 import CartProductListItemExtended from 'components/CartProductList/CartProductListItemExtended'
 import CartTotal from 'components/CartTotal/CartTotal'
+import { Link } from 'react-router-dom'
 import { useAppSelector } from 'redux/hooks'
 
 const CartPage = () => {
@@ -22,6 +23,7 @@ const CartPage = () => {
                 />
             </Grid>
             <CartTotal productsInCart={productsInCart} />
+            <Link to="/checkout">Proceed to Checkout</Link>
         </div>
     )
 }
